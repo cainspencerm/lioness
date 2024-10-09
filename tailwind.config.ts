5ed2a488caab8c3dabbd9157170dc027b41e2191
Spencer Cain <cainspencerm@protonmail.com>
2024-10-09 13:58:45 -0400

    chore: :tada: initialize tauri with nextjs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src-next/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src-next/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src-next/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -15,5 +15,5 @@ const config: Config = {
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
