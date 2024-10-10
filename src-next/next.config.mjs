dd2b956a58e478b2f938598c07fdca0a72e978d1
Spencer Cain <cainspencerm@protonmail.com>
2024-10-09 20:47:35 -0400

    chore(repo): :sparkles: prepare repository

--- src-next/next.config.mjs
+++ src-next/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  swcMinify: true,
+  output: "export",
+  distDir: "../dist",
+}
 
-export default nextConfig;
+export default nextConfig
